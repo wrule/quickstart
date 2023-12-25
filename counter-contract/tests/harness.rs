4433c77964c0a3fb6684c6b7fc08d4173a5ea7db
jimao <jimao@jimaodeMacBook-Pro.local>
2023-12-25 18:02:04 +0800

    test_increment测试

--- counter-contract/tests/harness.rs
+++ counter-contract/tests/harness.rs
@@ -49,3 +49,18 @@ async fn can_get_contract_id() {
     // Recall that the initial value of the counter was 0.
     assert_eq!(result.value, 1);
 }
+
+#[tokio::test]
+async fn test_increment() {
+    let (instance, _id) = get_contract_instance().await;
+ 
+    // Increment the counter
+    instance.methods().increment().call().await.unwrap();
+ 
+    // Get the current value of the counter
+    let result = instance.methods().count().call().await.unwrap();
+ 
+    // Check that the current value of the counter is 1.
+    // Recall that the initial value of the counter was 0.
+    assert_eq!(result.value, 1);
+}
